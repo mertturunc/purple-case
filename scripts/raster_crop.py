32ebfb691d0b0a65bac03da6870977866c9af1eb
mertturunc <mertturunc@gmail.com>
2025-07-13 16:22:22 +0300

    changing sat img from palsar to srtm

--- scripts/raster_crop.py
+++ scripts/raster_crop.py
@@ -1,39 +1,35 @@
-# this script is used to crop the raster to the bounding box
+# this script is used to merge and crop rasters to the bounding box
+# alos palsar didn't work, so i used srtm
 
 import rasterio
-from shapely.geometry import box
-import geopandas as gpd
+from rasterio.merge import merge
 from rasterio.mask import mask
+from shapely.geometry import box
+import glob
+import os
 
-# input raster (ALOS PALSAR Level 2 GeoTIFF)
-input_raster = "./raster/HH-ALPSRP228400790-H2.2_UA.tif"
+os.environ['PROJ_LIB'] = r"D:\getir-case\.venv\Lib\site-packages\rasterio\proj_data"
 
-# bounding box (WGS84)
-bbox_wgs84 = (28.753995, 40.108817, 29.319104, 40.336879)  # (minx, miny, maxx, maxy)
 
-output_raster = "./data/cropped_alos_palsar.tif"
+bbox = box(28.753995, 40.108817, 29.319104, 40.336879)  # WGS84 bbox
+input_folder = "./raster"
+output_raster = "./data/working_area_srtm.tif"
 
-# source: https://rasterio.readthedocs.io/en/stable/api/rasterio.mask.html
-with rasterio.open(input_raster) as src:
-    print("raster bounds:", src.bounds)
-    print("raster CRS:", src.crs)
-    # bbox to box
-    bbox_geom = box(*bbox_wgs84)
-    # gdf as 4326
-    gdf = gpd.GeoDataFrame({'geometry': [bbox_geom]}, crs="EPSG:4326")
-    # 4326 to utm
-    gdf_proj = gdf.to_crs(src.crs)
-    # mask
-    shapes = [gdf_proj.iloc[0].geometry]
-    out_image, out_transform = mask(src, shapes, crop=True)
-    out_meta = src.meta.copy()
-    out_meta.update({
-        "driver": "GTiff",
-        "height": out_image.shape[1],
-        "width": out_image.shape[2],
-        "transform": out_transform
-    })
-    with rasterio.open(output_raster, "w", **out_meta) as dest:
-        dest.write(out_image)
+files = glob.glob(os.path.join(input_folder, "*.tif"))
+srcs = [rasterio.open(f) for f in files]
+mosaic, out_trans = merge(srcs)
+meta = srcs[0].meta.copy()
+for src in srcs: src.close()
+
+meta.update({"driver": "GTiff", "height": mosaic.shape[1], "width": mosaic.shape[2], "transform": out_trans})
+
+with rasterio.MemoryFile() as memfile:
+    with memfile.open(**meta) as dataset:
+        dataset.write(mosaic)
+        bbox_proj = rasterio.warp.transform_geom("EPSG:4326", dataset.crs, bbox.__geo_interface__)
+        out_image, out_transform = mask(dataset, [bbox_proj], crop=True)
+        meta.update({"height": out_image.shape[1], "width": out_image.shape[2], "transform": out_transform})
+        with rasterio.open(output_raster, "w", **meta) as dest:
+            dest.write(out_image)
 
 print("raster kaydedildi:", output_raster)
